a0448fc35d5a879a0fbce6eeacfd9fde3ebd3aaa
Mischa02011990 <111740472+Mischa02011990@users.noreply.github.com>
2023-01-11 15:33:03 +0200

    Updates

---
+++ precache-manifest.7b4780b5fa752d479f431adbb18e05d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901f90781e78a325eed4b8f2084ead42",
+    "url": "/DZ-IGNAT/index.html"
+  },
+  {
+    "revision": "df4c101d973f30ed3e79",
+    "url": "/DZ-IGNAT/static/css/main.58fa02d5.chunk.css"
+  },
+  {
+    "revision": "a3a19f3debfee991ebd0",
+    "url": "/DZ-IGNAT/static/js/2.d47564e7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/DZ-IGNAT/static/js/2.d47564e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4c101d973f30ed3e79",
+    "url": "/DZ-IGNAT/static/js/main.b28d052c.chunk.js"
+  },
+  {
+    "revision": "064ef29d8ee43cd07c31",
+    "url": "/DZ-IGNAT/static/js/runtime-main.4a21612c.js"
+  }
+]);
